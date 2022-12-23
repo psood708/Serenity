2baf2b11d15730652513b385858c7bd724f609b4
Parth <parth.sce21@sot.pdpu.ac.in>
2022-12-23 20:44:58 +0530

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -7,17 +7,28 @@ import { useSelector } from 'react-redux';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import {createTheme} from "@mui/material/styles";
 import { themeSettings } from 'theme';
+
+
+// we have created the jsconfig.json to make it easier in imports as we see here in scenes
 function App() {
+  
+  const mode = useSelector((state)=>state.mode);
+  const theme = useMemo(()=> createTheme(themeSettings(mode)),[mode]);
+
+  
+  
   return (
   <div className='app'>
   
   <BrowserRouter>
+  <ThemeProvider theme = {theme}>
+    <CssBaseline />
     <Routes>
       <Route path="/" element={<LoginPage/>}/>
       <Route path="/home" element={<HomePage/>}/>
       <Route path="/profile/:userId" element={<ProfilePage/>}/>
-
     </Routes> 
+    </ThemeProvider>
   </BrowserRouter>
   </div>
   );
